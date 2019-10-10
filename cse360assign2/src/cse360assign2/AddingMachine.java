c323d7408c65613660d16e2499d3beee430baf06
alan0 <alan0@DESKTOP-NRB6E22.Home>
2019-10-09 23:20:03 -0700

    intial checking of AddingMachine

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,40 @@
+/**
+ * --------------------------------------------------------------------------------------------
+ * NAME         : ALAN THOMAS PAUL MUNDADAN
+ * CLASS        : CSE 360
+ * DATE         : 10/09/2019
+ * CLASS NUMBER : 70642
+ * PURPOSE      : PERFORM THE FUNCTIONS OF A BASIC CALCULATOR
+ * --------------------------------------------------------------------------------------------               
+ */
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	//--RETURNS THE TOTAL
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	//--FUNCTION TO ADD VALUE
+	public void add (int value) {
+		
+	}
+	//--FUNCTION TO SUBTRACT VALUE
+	public void subtract (int value) {
+		
+	}
+	//--FUNTION TO CONVERT TO STRING	
+	public String toString () {
+		return "";
+	}
+    //--FUNCTION TO THE CLEAR THE DATA
+	public void clear() {
+	
+	}
+}
