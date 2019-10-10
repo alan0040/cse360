fcf5ec087169b153252b6919d1cddf81f42da41c
alan0 <alan0@DESKTOP-NRB6E22.Home>
2019-10-09 23:45:12 -0700

    Updated AddingMachine

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -13,28 +13,35 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String ophistory = "0";
 	//--RETURNS THE TOTAL
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	//--FUNCTION TO ADD VALUE
 	public void add (int value) {
+		total = total+value;
+		ophistory = ophistory + " + " + value;
 		
 	}
 	//--FUNCTION TO SUBTRACT VALUE
 	public void subtract (int value) {
+		total = total - value;
+		ophistory = ophistory + " - " + value;
 		
 	}
 	//--FUNTION TO CONVERT TO STRING	
 	public String toString () {
-		return "";
+		return ophistory;
 	}
     //--FUNCTION TO THE CLEAR THE DATA
 	public void clear() {
+		total = 0;
+		ophistory = "";
 	
 	}
 }
